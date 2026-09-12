ea4c83f122df87f299f335e1029070fb02256d94
bibekdotdev <bibekjana68@gmail.com>
2026-09-12 11:24:52 +0530

    add

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ async function connectDB() {
 }
 
 connectDB();
-const PORT = 8080;
+const PORT = "https://birthday-qou0.onrender.com" || 8080;
 app.listen(PORT, () => {
   console.log("hi");
   console.log("hi");
